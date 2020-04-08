18ced38401b4d76ac23ddcf9285ff3fe205f2b49
user2 <user2@DESKTOP-BQDDAC1>
2020-04-08 19:00:04 +0900

    image파일 위치 수정
    파일위치수정

--- src/com/kh/team/menu/StartMenu.java
+++ src/com/kh/team/menu/StartMenu.java
@@ -14,12 +14,12 @@ public class StartMenu extends SuperMenu
 		
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0 , 500, 500);
-		Image background = new ImageIcon("C:\\Users\\tmddu\\git\\OZ\\src\\com\\kh\\team\\image\\start\\KakaoTalk_20200408_141935784.png").getImage().getScaledInstance(100, 100, 0);
+		Image background = new ImageIcon().getImage().getScaledInstance(100, 100, 0);
 		
 		JLabel label = new JLabel(new ImageIcon(background));
 		
 		JPanel startPanel = new JPanel();
-		ImageIcon start = new ImageIcon("C:\\Users\\tmddu\\git\\OZ\\src\\com\\kh\\team\\image\\start\\KakaoTalk_20200408_141320536.png");
+		ImageIcon start = new ImageIcon();
 		//JButton button = new JButton(start);
 		//button.setBounds(0, 0, 30, 20);
 		panel.add(label);
